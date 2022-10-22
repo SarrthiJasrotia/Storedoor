c12336a8c642bb055d85b27ddc0cee89b487684a
Sarrthi <sarrthijasrotia05@gmail.com>
2022-10-21 21:05:17 -0500

    static, template,urls folders and files made

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Store 
+def store(request):
+    context = {}
+    return render(request, 'store/store.html')
+#cart
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html')
+
