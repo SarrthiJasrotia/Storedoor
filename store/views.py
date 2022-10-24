0e4fa7573de7b3d5b5cdd672909f5d0eb8200071
Sarrthi <sarrthijasrotia05@gmail.com>
2022-10-24 11:07:11 -0500

    css frame work deleted, nav made, item cards made

--- store/views.py
+++ store/views.py
@@ -1,9 +1,11 @@
+from itertools import product
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 #Store 
 def store(request):
+    product = Product.objects.all()
     context = {}
     return render(request, 'store/store.html')
 #cart
