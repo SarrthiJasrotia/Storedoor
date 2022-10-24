7f1bbf434fa1033379df727385b3ad14564eeb2d
Sarrthi <sarrthijasrotia05@gmail.com>
2022-10-24 09:54:12 -0500

    models made for Customer, product, order

--- store/models.py
+++ store/models.py
@@ -1,3 +1,38 @@
+from audioop import mul
+from contextlib import nullcontext
+from email.policy import default
+from tokenize import blank_re
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True)
+    name = models.CharField(max_length=200, null= True)
+    email = models.CharField(max_length=200, null= True)
+
+    def __str__(self) :
+        return self.name
+
+class Product(models.Model):
+     name = models.CharField(max_length=200, null= True)
+     price = models.FloatField()
+     digital = models.BooleanField(default=False, null= True, blank=False)
+
+     def __str__(self) :
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank= True, null=True)
+    date_ordered = models.DateField(auto_now_add = True)
+    complete = models.BooleanField(default=False, null= True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItemn(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank= True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank= True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank =True)
+    date_added = models.DateField(auto_now_add = True)
