8f8503c458e453a45cb98720d026386d09acb2dd
Sarrthi <sarrthijasrotia05@gmail.com>
2022-10-25 14:57:35 -0500

    add and remove updates the cart item qty

--- static/js/cart.js
+++ static/js/cart.js
@@ -24,8 +24,35 @@ for( let i = 0; i < updateBtn.length; i++){
         if (user ==="AnonymousUser"){
             console.log('not logged in')
         }else{
-            console.log('user is logged in')
+            updateUserOrder(productId, action)
         }
         
     })
+}
+
+
+function updateUserOrder(productId,action){
+    console.log('User is logged in ')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action':action})
+    })
+    .then((response)=>{
+        return response.json()
+
+    })
+    .then((data)=>{
+        
+        console.log('data',data)
+        
+    })
+
+
 }
