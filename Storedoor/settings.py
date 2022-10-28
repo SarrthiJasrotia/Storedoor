a35ffc302bc0e1a02ace9c8a159eea8c3b6ec103
Sarrthi <sarrthijasrotia05@gmail.com>
2022-10-27 20:12:40 -0500

    de

--- Storedoor/settings.py
+++ Storedoor/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,12 +117,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/images/'
